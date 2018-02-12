c8888d535c062676df9861d97c50499ce226d71c
张建 <jian.zhang@yuntu-inc.com>
2018-02-12 22:29:55 +0800

    First Commit

---
+++ ZJMaricle/Classes/ZJTest.h
@@ -0,0 +1,13 @@
+//
+//  ZJTest.h
+//  RuleTest
+//
+//  Created by 张建 on 2018/2/12.
+//  Copyright © 2018年 张建. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface ZJTest : NSObject
+
+@end
---
+++ ZJMaricle/Classes/ZJTest.m
@@ -0,0 +1,13 @@
+//
+//  ZJTest.m
+//  RuleTest
+//
+//  Created by 张建 on 2018/2/12.
+//  Copyright © 2018年 张建. All rights reserved.
+//
+
+#import "ZJTest.h"
+
+@implementation ZJTest
+
+@end
